882b98147cb71ca4ff6f6a2b0f2d19145040aae1
Kateryna Hrinchak <92380648+KatyaHrinchak@users.noreply.github.com>
2026-03-05 23:21:38 +0200

    feat(ecr):CLT-76 add ECR for docker images

---
+++ global/ecr/main.tf
@@ -0,0 +1,8 @@
+resource "aws_ecr_repository" "cli-team-ecr" {
+  name                 = "cli-team-ecr"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
---
+++ global/ecr/outputs.tf
@@ -0,0 +1,3 @@
+output "repository_url" {
+  value = aws_ecr_repository.cli-team-ecr.repository_url
+}
