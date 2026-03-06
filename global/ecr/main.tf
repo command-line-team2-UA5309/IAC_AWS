9d54328e197a29933ef2ad71d6a06f56706b0db9
Kateryna Hrinchak <92380648+KatyaHrinchak@users.noreply.github.com>
2026-03-06 10:33:15 +0200

    chore(ecr):CLT-76 remove scanning

--- global/ecr/main.tf
+++ global/ecr/main.tf
@@ -1,8 +1,4 @@
 resource "aws_ecr_repository" "cli-team-ecr" {
   name                 = "cli-team-ecr"
   image_tag_mutability = "IMMUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
 }
